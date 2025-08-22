478e3998c044a4d2be299cd98273d51dcc0be1cf
muzammil16712 <fatimasha4973@gmail.com>
2025-08-22 09:45:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
 const express = require('express');
-const bodyParser = require("body-parser");
-const path = require("path");
-
 const app = express();
+__path = process.cwd()
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-
-// Import routes
-const server = require('./qr');
-const code = require('./pair');
-
-// Increase listener limit
+let server = require('./qr'),
+    code = require('./pair');
 require('events').EventEmitter.defaultMaxListeners = 500;
-
-// Middlewares
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Routes
 app.use('/server', server);
 app.use('/code', code);
-
-app.get('/pair', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'pair.html'));
-});
-
-app.get('/qr', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'qr.html'));
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(process.cwd(), 'main.html'));
-});
-
-// Start server
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/qr',async (req, res, next) => {
+res.sendFile(__path + '/qr.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/main.html')
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(PORT, () => {
-  console.log(`
-✅ Server running on http://localhost:${PORT}
-⭐ Don't forget to give a star!
-  `);
-});
+    console.log(`
+Don't Forgot To Give Star
 
-module.exports = app;
+ Server running on http://localhost:` + PORT)
+})
+
+module.exports = app
